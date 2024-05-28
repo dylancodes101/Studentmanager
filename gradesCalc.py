9ed3205bbaf30818ea0f11935b21fc8269504403
dylancodes101 <64162581+dylancodes101@users.noreply.github.com>
2024-05-28 15:50:23 -0400

    Add files via upload

---
+++ gradesCalc.py
@@ -0,0 +1,10 @@
+#weighted average calculator
+class Grades:
+    rubric = {}
+    grades = {}
+    def add_attribute(self, attr_name, attr_value):
+        setattr(self, attr_name, attr_value)
+    def curveGenerator():
+        None 
+    def average(grades):
+        
