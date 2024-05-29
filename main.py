5bf6bf0728a474aed4d9922b8470ee62c22e963f
dylancodes101 <64162581+dylancodes101@users.noreply.github.com>
2024-05-29 02:18:41 +0000

    major changes

--- main.py
+++ main.py
@@ -5,48 +5,20 @@ import random as rm
 from tabulate import tabulate
 import names as nm
 import input as IP
-grades = ["A","B","C","D","F"]
+from fake_data import generate_fake_students
+from csv_operations import save_students_to_csv, read_students_from_csv
+def main():
+    amount_of_students = IP.Input().fake_students_amount()
+    Students = generate_fake_students(amount_of_students)
+    if Students:
+        save_students_to_csv(Students,"students.csv")
+    data = read_students_from_csv("students.csv")
+    
+if __name__ == "__main__":
+    Students = main()
 
 
-class Student:
-    def __init__(self, first_name, last_name, age):
-        self.first_name = first_name
-        self.last_name = last_name
-        self.age = age
-        self.student_id = self.generate_unique_id()
 
-    def generate_unique_id(self):
-        unique_str = f"{self.first_name}{self.last_name}{self.age}{time.time()}"
-        #hashed_str = hashlib.sha256(unique_str.encode()).hexdigest()
-        return unique_str
-
-    def add_attribute(self, attr_name, attr_value):
-        setattr(self, attr_name, attr_value)
-
-    def get_attributes(self):
-        return self.__dict__
-
-students = [] 
-
-def generate_fake_students(students,amount):
-    for i in range(int(amount)):
-        student = Student(nm.Names().generate_name_last(),nm.Names().generate_name_first(), rm.randint(6, 13))
-        students.append(student)
-        students[i].add_attribute('letter_grade', rm.choice(grades))
-
-def save_students_to_csv(students, filename):
-    with open(filename, 'w', newline='') as csvfile:
-        fieldnames = students[0].get_attributes().keys()  # Get field names from the first student
-        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-
-        writer.writeheader()
-        for student in students:
-            writer.writerow(student.get_attributes())
-
-generate_fake_students(students,IP.Input().fake_students_amount())
-
-# Save students to a CSV file
-save_students_to_csv(students, 'students.csv')
 
 # Function to dynamically generate the student table
 def generate_student_table(students):
@@ -105,7 +77,7 @@ def find_student_by_attr(students,attr, attr_val):
         print("Student not found or invalid input.")
 
 # Example usage:
-headers, student_table = generate_student_table(students)
+headers, student_table = generate_student_table(Students)
 print(tabulate(student_table, headers=headers))
 #add the ability to search for a student by any value
 #sort ability as well
---
+++ student.py
@@ -0,0 +1,32 @@
+# person.py
+import time
+class Student:
+    def __init__(self, last_name, first_name, age=None):
+        self.last_name = last_name
+        self.first_name = first_name
+        self.age = age
+
+    def add_attribute(self, attr_name, attr_value):
+        setattr(self, attr_name, attr_value)
+    def generate_unique_id(self):
+        unique_str = f"{self.first_name}{self.last_name}{self.age}{time.time()}"
+        #hashed_str = hashlib.sha256(unique_str.encode()).hexdigest()
+        return unique_str
+
+    def get_attributes(self):
+        return self.__dict__
+
+    class DOB:
+        def __init__(self, month, day, year, format=None):
+            self.month = month
+            self.day = day
+            self.year = year
+            self.format = format
+
+        def __str__(self):
+            return f"{self.month:02d}/{self.day:02d}/{self.year}"
+
+        def get_attributes(self):
+            return {'month': self.month,
+                    'day': self.day,
+                    'year': self.year}
