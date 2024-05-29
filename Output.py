a93a3ec1862b96b82652e35f26ab8bb9caa72487
dylancodes101 <64162581+dylancodes101@users.noreply.github.com>
2024-05-29 23:46:43 +0000

    Minor fixes

---
+++ Output.py
@@ -0,0 +1,37 @@
+from tabulate import tabulate
+
+
+#students:the class of students
+#data the actuall dict storing the info
+#gets the attribute for the class and updates the set
+# a set() (basically an easy dict)https://chatgpt.com/share/7ea61115-a859-4486-a0cb-923cb8c363f4
+def generate_output(students, data):
+    headers = set()
+    
+    for student in students:
+        headers.update(student.get_attributes().keys())
+    headers = sorted(headers)
+
+    header_table = [(i + 1, header) for i, header in enumerate(headers)]
+    print(tabulate(header_table, headers=["#", "Attribute"]))
+
+
+
+
+
+
+
+
+
+
+
+
+    """#sel = int(
+        #input("Sort by which attribute (enter corresponding number): ")) - 1
+    #sort_attribute = header_table[sel][1]
+
+    sorted_data = sorted(data,
+                         key=lambda x: int(x[sort_attribute])
+                         if x[sort_attribute].isdigit() else x[sort_attribute],
+                         reverse=True)
+    #print(tabulate(sorted_data, headers='keys', tablefmt='grid'))"""
--- main.py
+++ main.py
@@ -3,16 +3,15 @@ from tabulate import tabulate
 import input as IP
 from fake_data import generate_fake_students
 from csv_operations import save_students_to_csv,read_students_from_csv
+import Output
 
-def generate_students_table(students, data):
-  global headers
+"""def generate_students_table(students, data):
   temp = []
   headers = set()
 
   for student in students:
       headers.update(student.get_attributes().keys())
   headers = sorted(headers)
-
   for i, header in enumerate(headers):
       temp.append([i + 1, header])
   print(tabulate(temp, headers=["#", "Attribute"]))
@@ -22,14 +21,17 @@ def generate_students_table(students, data):
 
   sorted_data = sorted(data, key=lambda x: int(x[sort_attribute]) if x[sort_attribute].isdigit() else x[sort_attribute], reverse=True)
   print(tabulate(sorted_data, headers='keys', tablefmt='grid'))
-  
+  """
 def main():
   amount_of_students = IP.Input().fake_students_amount()
   students = generate_fake_students(amount_of_students)
   if students:
       save_students_to_csv(students, "students.csv")
   data = read_students_from_csv("students.csv")
-  generate_students_table(students, data)
+  #print(data)
+  #print(students)
+  #generate_students_table(students, data)
+  #Output.generate_output(students, data)
   
 if __name__ == "__main__":
   main()
